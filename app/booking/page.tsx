0c2296d295c785a23479af5f10fa217216fb915a
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2024-01-20 13:58:22 +0300

    first commit

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,10 +1,18 @@
 "use client"
+import { ThemeProvider, createTheme } from "@mui/material"
 import Booking from "./booking"
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#029E9D",
+        },
+    },
+});
 export default function Page() {
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <Booking />
-        </>
+        </ThemeProvider>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
-"use client"
+"use client";
 
-import {Footer, Header, Orders} from "@/components"
-import {ToastContainer, toast} from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import "./globals.css"
+import { Footer, Header, Orders } from "@/components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
 
-import {usePathname} from "next/navigation"
-import CurrentDate from "@/components/Date/page"
-import { useEffect, useState } from "react"
+import { usePathname } from "next/navigation";
+import CurrentDate from "@/components/Date/page";
+import { useEffect, useState } from "react";
+import { ThemeProvider, createTheme } from "@mui/material";
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#029E9D",
+        },
+    },
+});
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const pathname = usePathname();
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
-    const pathname = usePathname()
-
-        return (
+    return (
         <html lang="en">
             <body>
                 <main>
@@ -20,18 +31,18 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
                     {["/login", "/register"].includes(pathname) ? (
                         children
                     ) : (
-                        <>
+                        <ThemeProvider theme={theme}>
                             <div className="h">
                                 <CurrentDate />
-                                <Header/>
+                                <Header />
                             </div>
                             {children}
                             <Orders />
                             <Footer />
-                        </>
+                        </ThemeProvider>
                     )}
                 </main>
             </body>
         </html>
-    )
+    );
 }
